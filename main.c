2b1de69a39a97b10b34bcfcf6b2106252b9443b7
giygas-on-gba <63275899+giygas-on-gba@users.noreply.github.com>
2024-04-17 21:48:32 -0300

    Add files via upload

---
+++ main.c
@@ -0,0 +1,71 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char *argv[])  {
+	// lista com descritor
+	struct descritor{
+		int n;
+		struct no *i,*f;
+	};
+	struct no{
+		int dado;
+		struct no *prox;
+	};
+	struct no *p,*r,*lista; //auxiliar a criar nos e descritores
+	struct descritor *d; 
+	
+	 //lista LSE vazia
+
+	//deve-se criar o descritor
+	p=malloc(sizeof(struct descritor));
+	//agora tem-se uma lista com campo i,n,f
+	d->i=NULL;
+	d->f=NULL;
+	d->n=0;
+	
+	/* Nota: só para demonstracao, sera 
+	//campos i,n,f estão vazios
+	//insercao do primeiro no
+	p=malloc(sizeof(struct no));
+	scanf("%d",&p->dado);
+	p->prox=NULL; // Só tem um nó por enquanto e "p" sabe a pos. dele
+	d->i=p; // p sabe pós
+	d->f=p;
+	d->n=1; // num. do nó
+	*/
+	
+	//ins. a direita
+	// agora diferentemente da lse simples, o f vai apontar para o novo nó.
+	do{
+		
+		//perguntar se a lista esta vazia
+		//se n=0 a lista esta vazia
+		p=malloc(sizeof(struct no));
+		scanf("%d",&p->dado);
+		p->prox=NULL;// Só tem um nó por enquanto e "p" sabe a pos. dele
+		if (d->n=0){
+			d->i=p; // p sabe pós.
+			d->f=p;
+			d->n=1; // num. do nó
+		}
+		else{
+			q=d->f; //quer que q aponte para o mesmo nó que o d->f aponta.
+			q->prox=p; // p é o ultimo nó , e o prox de q agora aponta para ele
+			d->f=p; // p é o ultimo nó então f agora aponta para ele inves de q (era o unico item na lista).
+			d->n++; // n é incrementado já que um nó foi adicionado, agora tem 2.
+			//note que o n não tem nada a ver com os ponteiros i e f, então não use ele na remoção de nós.
+			
+		}
+		
+		printf("Deseja inserir outro no a direita? 0-nao 1-sim");
+		scanf("%d",&sn)
+	}while(sn==1);
+	
+
+	
+	
+	
+	return 0
+}
